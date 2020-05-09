8a5eefaffa2a49d8f1ca3368997f7fba872a3d70
prasanna-srikanth <57836156+prasanna-srikanth@users.noreply.github.com>
2020-05-09 21:55:22 +0100

    Initial Commit

---
+++ SampleSerilogApi/Controllers/WeatherForecastController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Serilog;
+
+namespace SampleSerilogApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            _logger.LogDebug("Get method is called");  
+
+            Log.Information($"Get method called at {DateTime.Now}");
+
+            try
+            {
+                var rng = new Random();
+                return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+                {
+                    Date = DateTime.Now.AddDays(index),
+                    TemperatureC = rng.Next(-20, 55),
+                    Summary = Summaries[rng.Next(Summaries.Length)]
+                })
+                .ToArray();
+            }
+            catch(Exception ex)
+            {
+                _logger.LogError($"Error occurred in Get method - {ex}");
+                throw;
+            }
+        }
+    }
+}
